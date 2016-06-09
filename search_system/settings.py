32a807d3f94dd03d00398dc447c7c7290482a278
Anton <Anton.Dacik@gmail.com>
2016-06-10 00:32:52 +0300

    asd

--- search_system/settings.py
+++ search_system/settings.py
@@ -157,7 +157,7 @@ LOGGING = {
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = './../search/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )

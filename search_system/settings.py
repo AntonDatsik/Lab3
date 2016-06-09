e0de30fb1b9e117e77a31192d0cda0be55dae654
Anton <Anton.Dacik@gmail.com>
2016-06-10 00:42:10 +0300

    asd

--- search_system/settings.py
+++ search_system/settings.py
@@ -153,6 +153,10 @@ LOGGING = {
     },
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
